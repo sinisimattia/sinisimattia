0e619912607cf5509802daeb7a93912f6b9289b4
Mattia Sinisi <mattia@snisni.it>
2021-07-15 15:53:35 +0200

    Created synchronized sliders for sites and their domains

---
+++ animations.js
@@ -0,0 +1,47 @@
+var typed = new Typed('#type', {
+	strings: [
+		"I like to develop open source.",
+		"I like to write about programming.",
+		"I like to try new stuff.",
+		"I like to experiment.",
+		"Let's talk!"
+	],
+	typeSpeed: 30,
+	smartBackspace: true,
+	backSpeed: 30,
+	backDelay: 1000,
+	cursorChar: '|',
+	loop: true,
+	startDelay: 3000,
+
+});
+
+var domainsCarousel = new Splide( '#domains-carousel', {
+	height: '4rem',
+	direction: 'ttb',
+	arrows: false,
+	pagination: false,
+	drag: false,
+	autoplay: true,
+	interval: 5000,
+	pauseOnHover: true,
+	pauseOnFocus: true,
+	isNavigation: true,
+});
+
+var sitesCarousel = new Splide( '#sites-carousel', {
+	type: 'loop',
+	height: '500px',
+	arrows: false,
+	pagination: false,
+	autoplay: true,
+	interval: 5000,
+	pauseOnHover: true,
+	pauseOnFocus: true,
+	perPage:3,
+	gap: 20,
+});
+
+domainsCarousel.sync(sitesCarousel).mount();
+
+sitesCarousel.mount();
