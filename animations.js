344d3a940d614d1297f63808941b38fb3b3ca2fa
Mattia Sinisi <mattia@snisni.it>
2021-09-04 12:42:06 +0200

    WIP: Removed splide nad tweaked terminal animation

--- animations.js
+++ animations.js
@@ -11,50 +11,6 @@ var typed = new Typed('#type', {
 	backSpeed: 30,
 	backDelay: 1000,
 	cursorChar: '|',
-	loop: true,
-	startDelay: 3000,
-
+	loop: false,
+	startDelay: 500,
 });
-
-var domainsCarousel = new Splide( '#domains-carousel', {
-	type: 'loop',
-	height: '3.5rem',
-	fixedWidth: '240px',
-	direction: 'ttb',
-	reverse: true,
-	arrows: false,
-	pagination: false,
-	drag: false,
-	autoplay: true,
-	interval: 5000,
-	pauseOnHover: true,
-	pauseOnFocus: true,
-	isNavigation: true,
-});
-
-var sitesCarousel = new Splide( '#sites-carousel', {
-	type: 'loop',
-	height: '500px',
-	direction: 'rtl',
-	focus: 'center',
-	arrows: false,
-	pagination: false,
-	autoplay: true,
-	interval: 5000,
-	pauseOnHover: true,
-	pauseOnFocus: true,
-	perPage: 3,
-	gap: 20,
-	breakpoints: {
-		1000: {
-			perPage: 2,
-		},
-		700: {
-			perPage: 1
-		}
-	}
-});
-
-domainsCarousel.sync(sitesCarousel).mount();
-
-sitesCarousel.mount();
