bc5808e32ec6d4512561a24d5101d7c734640bc1
Mattia Sinisi <m.sinisi@key2.it>
2021-11-02 11:40:42 +0100

    Using Alpine.js for links

---
+++ data.js
@@ -0,0 +1,22 @@
+const links = [
+	{
+		name: "blog.mattia.codes",
+		descr: "A blog about programming OSS",
+		url: "https://blog.mattia.codes",
+	},
+	{
+		name: "work.mattia.codes",
+		descr: "My online curriculum",
+		url: "https://work.mattia.codes",
+	},
+	{
+		name: "talk.mattia.codes",
+		descr: "A way for you to contact me",
+		url: "https://talk.mattia.codes",
+	},
+	{
+		name: "projects.mattia.codes",
+		descr: "A collection of personal projects",
+		url: "https://projects.mattia.codes",
+	},
+];
